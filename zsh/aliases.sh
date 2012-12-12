46b3be8fb0b86bcf8f4df70acebec1bc66e1caf7
Michael Schnelle <michael.schnelle.ms@gmail.com>
2012-12-12 16:08:02 +0100

    custom commands added

---
+++ zsh/aliases.sh
@@ -0,0 +1,8 @@
+# aliases for changing directories
+
+alias cd..='cd ..'
+alias c2='cd ../..'
+alias c3='cd ../../..'
+alias c4='cd ../../../..'
+alias c5='cd ../../../../..'
+alias c6='cd ../../../../../..'
